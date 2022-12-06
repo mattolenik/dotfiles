4b0ed6bf9debcf8dd2dee358c14015e7f74a94f4
Matt Olenik <olenikm@gmail.com>
2022-12-06 12:28:36 -0800

    Fix fzf loading, add fzf-tmux

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/matt/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/matt/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/matt/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/matt/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
