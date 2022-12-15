d0fb546ed13558fdc5d85fa452af7cbb41db7f6d
Matt Olenik <olenikm@gmail.com>
2022-12-15 11:59:16 -0800

    Improve ctrl t command, config cleanup

--- zsh/plugins/fzf/fzf.plugin.zsh
+++ zsh/plugins/fzf/fzf.plugin.zsh
@@ -1,16 +1,40 @@
 # vi: ft=zsh
 
-if command_exists rzf || ! command_exists rg; then
+if ! command_exists fzf || ! command_exists rg; then
   print "WARNING: fuzzy finder setup requires fzf and ripgrep to be installed:\nhttps://github.com/junegunn/fzf\nhttps://github.com/BurntSushi/ripgrep"
 fi
 
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+FZF_DIR="$HOME/.fzf"
 
-source "$HOME/.fzf/shell/key-bindings.zsh"
-source ~/.fzf.zsh
+[[ $- == *i* ]] && source "$FZF_DIR/shell/completion.zsh" 2> /dev/null
+source "$FZF_DIR/shell/key-bindings.zsh"
+
+declare -a ctrl_t_globs
+declare -a ctrl_t_args
+
+ctrl_t_globs=(
+  '!go/pkg'
+)
+
+if is_macos; then
+  ctrl_t_globs+=(
+    '!Applications (Parallels)'
+    '!Applications/'
+    '!Library'
+    '!Parallels'
+    '!Pictures/*.photoslibrary'
+  )
+fi
+
+for glob in $ctrl_t_globs; do
+  ctrl_t_args+=(-g "'$glob'")
+done
 
 export FZF_TMUX_OPTS="-d"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-export FZF_CTRL_T_COMMAND='rg --files --smart-case --glob "!Applications/" --glob "!Applications (Parallels)" --glob "!Library"'
+export FZF_CTRL_T_COMMAND="rg --files --smart-case${ctrl_t_args:+ ${ctrl_t_args[@]}}"
 export FZF_DEFAULT_COMMAND='rg --files --hidden --smart-case --glob "!.git/*"'
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border --extended'
+
+source_if_exists "$HOME/.fzf.zsh"
+
